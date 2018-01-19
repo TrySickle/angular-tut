f3596b953dfb9938d7296c5f290cc0c07070bd2a
Jason Lee <jason.khn.lee@gmail.com>
2018-01-18 23:53:19 -0500

    Reads heroes from list, uses repeater directive

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Janna' },
+    { id: 12, name: 'Malzahar' },
+    { id: 13, name: 'Draven' },
+    { id: 14, name: 'Swain' },
+    { id: 15, name: 'Ashe' },
+    { id: 16, name: 'Ryze' },
+    { id: 17, name: 'Mordekaiser' },
+    { id: 18, name: 'Ziggs' },
+    { id: 19, name: 'Xerath' },
+    { id: 20, name: 'Viktor' },
+    { id: 21, name: 'Malphite' }
+];
